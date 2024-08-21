4fb313878bf564458fd68bd2b710ac4a37355a4f
Phong Vo <vhphong@gmail.com>
2024-08-21 16:44:39 -0500

    add security to gateway

--- src/main/java/com/pnk/profile_service/service/UserProfileService.java
+++ src/main/java/com/pnk/profile_service/service/UserProfileService.java
@@ -3,10 +3,14 @@ package com.pnk.profile_service.service;
 import com.pnk.profile_service.dto.request.UserProfileCreationRequest;
 import com.pnk.profile_service.dto.response.UserProfileResponse;
 
+import java.util.List;
+
 
 public interface UserProfileService {
 
     UserProfileResponse registerUserProfile(UserProfileCreationRequest userProfileCreationRequest);
+
+    List<UserProfileResponse> getAllUserProfiles();
 
     UserProfileResponse getUserProfileById(String id);
 }
